6946e367af5d607b28ab0563602c7957d2287a48
esafraaz <esaf.raaz@gamil.com>
2023-09-11 01:34:52 +0600

    first commit

---
+++ src/count.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export default function Counter () {
+    let [count , setCount] = useState(0)
+    const increment = () =>{
+        let newCount = count + 1;
+        let newCount1 = count++;
+        setCount(newCount1)
+    }
+    return (
+        <div>
+            <h3> Counter: {count}</h3>
+            <button onClick={increment}>Add</button>
+        </div>
+    )
+}
